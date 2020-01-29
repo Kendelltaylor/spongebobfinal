f3f6acaf6cade3e0ae7fccd0e5276fed1e9d0b17
Kendelltaylor <45106796+Kendelltaylor@users.noreply.github.com>
2020-01-29 10:13:44 -0800

    Add files via upload

---
+++ spongebobfinal.rb
@@ -0,0 +1,149 @@
+sample "C:/Users/Kendell_taylor/Documents/Audacity/And Now, Politics..wav"
+sleep 12.1
+use_bpm 160
+"C:/Users/Kendell_taylor/Documents/Audacity/im-ready2.wav"
+3.times do
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:E4
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:E5
+  sleep 1
+  play:Cs5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:Gs5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:B5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:Gs5
+  sleep 0.5
+  play:A5
+  sleep 1
+  play:E5
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:Gs4
+  sleep 1
+  play:Fs4
+  sleep 0.5
+  play:Gs4
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:B4
+  sleep 1
+  play:Cs5
+  sleep 0.5
+  play:D5
+  sleep 0.5
+  play:Cs5
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:E5
+  sleep 1
+  play:Ds5
+  sleep 0.5
+  play:E5
+  sleep 1
+  play:Fs5
+  sleep 0.5
+  play:Gs5
+  sleep 1
+  play:E5
+  sleep 0.5
+  play:D5
+  sleep 1
+  play:B4
+  sleep 0.5
+end
+sample "C:/Users/Kendell_taylor/Documents/Audacity/im-ready3.wav"
+sleep 5
+live_loop:all do
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:E4
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:E5
+  sleep 1
+  play:Cs5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:Gs5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:B5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:Gs5
+  sleep 0.5
+  play:A5
+  sleep 1
+  play:E5
+  sleep 0.5
+  play:Cs5
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:Cs5
+  sleep 0.5
+  play:D5
+  sleep 0.5
+  play:E5
+  sleep 0.5
+  play:A5
+  sleep 0.5
+  play:E5
+  sleep 0.5
+  play:D5
+  sleep 0.5
+  play:D5
+  sleep 1
+  play:B4
+  sleep 0.5
+  play:Gs4
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:B4
+  sleep 1
+  play:A4
+  sleep 0.5
+  play:A4
+  sleep 1
+  play:Gs4
+  sleep 0.5
+  play:A4
+  sleep 1.5
+  sample "C:/Users/Kendell_taylor/Documents/Audacity/Spongebob Theme Song 2.wav"
+  sleep 2
+end
+
